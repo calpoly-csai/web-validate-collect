51837a9a3d57a062e490c05a72f7450a5bd7baf7
mfekadu <mike.fekadu@gmail.com>
2019-07-13 18:45:33 -0400

    input bound to buttons that can be selected
    helpful resources
    
    https://www.vuemastery.com/pdf/Vue-Essentials-Cheat-Sheet.pdf
    
    https://www.w3schools.com/jsref/jsref_forin.asp
    
    https://stackoverflow.com/questions/36176073/what-is-vue-way-to-access-to-data-from-methods
    
    https://www.geeksforgeeks.org/javascript-split-a-string-with-multiple-separators/
    
    https://vuejs.org/v2/guide/class-and-style.html
    
    https://stackoverflow.com/questions/39336916/ausing-vue-js-to-duplicate-an-input-field-incrementing-the-index

--- src/views/Phrases.vue
+++ src/views/Phrases.vue
@@ -1,44 +1,105 @@
+<script>
+export default {
+  data: function() {
+    return {
+      categories: ["Building", "Office Hours", "Professor", "create_placeholder?"],
+      phrase: "",
+    }
+  },
+  computed: { // return cached values until dependencies change
+    tokens: function () {
+      return this.tokenize()
+    }
+  },
+  methods: {
+    tokenize: function() { /// given a string, output an array of words
+      let arr = this.phrase.split(/[ ?]/);
+      // remove the last one if it's the empty string
+      arr[arr.length - 1] == '' ? arr.pop() : null;
+      return arr;
+    }
+  }
+};
+</script>
+
+
 <template>
   <!-- TODO: change background -->
   <div class="page diagonal-background">
     <h1 class="page-title">Phrases</h1>
     <div class="dropdown-menu">
       <select>
-        <option value="cat1">Cat1</option>
-        <option value="cat2">Cat2</option>
-        <option value="cat3">Cat3</option>
-        <option value="cat4">Cat4</option>
+        <option v-for="(cat, index) in categories" v-bind:key="index">
+          {{ cat }}
+        </option>
       </select>
     </div>
     <div class="phrase-input-category-box is-centered">
       <div class="phrase-category-box">
-        <button class="phrase-category-button">cat1</button>
-        <button class="phrase-category-button">cat2</button>
-        <button class="phrase-category-button">cat3</button>
-        <button class="phrase-category-button">cat4</button>
+        <button class="phrase-category-button">Building</button>
+        <button class="phrase-category-button">Office Hours</button>
+        <button class="phrase-category-button">Professor</button>
+        <button class="phrase-category-button">create_placeholder?</button>
       </div>
+
       <div class="phrase-input-box">
-        <img src="../assets/start-quote.svg" alt="start-quote.svg">
+        <br>
+        <strong>Input Example:</strong>
+        <br><br>
         <input
           type="text"
-          placeholder="Phrase"
+          placeholder="Phrase Example"
           class="phrase-input"
+          v-model="phrase"
         >
+        <br><br>
+      </div>
+
+      <div v-if="this.phrase != ''" class="phrase-input-box">
+        <br>
+        <div><strong>Query:</strong></div>
+        <img src="../assets/start-quote.svg" alt="start-quote.svg">
+        <br>
+        <div>
+          <button v-for="(token, index) in this.tokens"
+                  v-bind:key="index"
+                  v-bind:class="{ 'data-button': true, 'selected': true }">
+            {{ token }}
+          </button>
+        </div>
         <img src="../assets/end-quote.svg" alt="end-quote.svg">
       </div>
+
     </div>
   </div>
 </template>
 
-<script>
-export default {};
-</script>
-
 <style>
 
+.data-button {
+  border: 2px solid black;
+  background-color: white;
+  color: black;
+  padding: 14px 28px;
+  margin: 0.5em;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 4px;
+  border-color: #2196F3;
+  color: dodgerblue
+}
+
+.data-button:hover {
+  background: #2196F3;
+  color: white;
+}
+
+.data-button.selected {
+  background: #2196F3;
+  color: white;
+}
 
 select {
-
   /* styling */
   background-color: white;
   border: thin solid blue;
@@ -109,6 +170,11 @@ select:-moz-focusring {
 
 .phrase-input-box {
   background-color: rgb(236,236,236);
+}
+
+.phrase-input {
+  width: 90%;
+  text-align: center;
 }
 
 .phrase-category-box {
